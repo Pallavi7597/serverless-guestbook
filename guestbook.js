104f721fafa45f9bc8225b336dcca0903321bf42
Pallavi7597 <66312599+Pallavi7597@users.noreply.github.com>
2022-03-14 15:34:27 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = "https://9ea04d57.us-south.apigw.appdomain.cloud/guestbook";
+const apiUrl = https://cbf12524.eu-gb.apigw.appdomain.cloud/guestbook;
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
